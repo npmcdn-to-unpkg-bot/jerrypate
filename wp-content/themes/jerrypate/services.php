0650ae551c4a813fbb9ac2ecd808009da413b9a7
Anne Grundhoefer <annegrundhoefer@gmail.com>
2016-04-25 20:23:12 -0500

    fixed services

--- wp-content/themes/jerrypate/services.php
+++ wp-content/themes/jerrypate/services.php
@@ -42,21 +42,21 @@ include('partials/header-pages.php'); ?>
                             <p>Jerry Pate Design is professionally licensed to practice landscape architecture in Florida, Alabama, and Mississippi.  Our focus is to create exterior spaces that support and enhance the architecture and context of the projects we engage in.  We seek to enhance the quality of life of those who experience our spaces.  We have experience working on projects with a wide range of uses—</p>
                             <div class="col_half nobottommargin">
                                 <ul class="list">
-                                    <li>commercial and retail projects</li>
-                                    <li>religious institutions</li>
-                                    <li>multifamily residential projects</li>
-                                    <li>community parks</li>
-                                    <li>medical facilities</li>
+                                    <li>Commercial and Retail Projects</li>
+                                    <li>Religious Institutions</li>
+                                    <li>Multifamily Residential Projects</li>
+                                    <li>Community Parks</li>
+                                    <li>Medical Facilities</li>
                                 </ul>
                             </div>
                               <div class="col_half col_last nobottommargin">
                                 <ul class="list">
-                                    <li>athletic facilities</li>
-                                    <li>academic campuses</li>
-                                    <li>streetscapes</li>
-                                    <li>stormwater amenities</li>
-                                    <li>assisted living facilities</li>
-                                    <li>play space</li>
+                                    <li>Athletic Facilities</li>
+                                    <li>Academic Campuses</li>
+                                    <li>Streetscapes</li>
+                                    <li>Stormwater Amenities</li>
+                                    <li>Assisted Living Facilities</li>
+                                    <li>Play Space</li>
                                 </ul>
                             </div>
 
@@ -104,7 +104,7 @@ include('partials/header-pages.php'); ?>
                             <p>Jerry Pate Design has been creating visually stunning and strategically creative golf courses for over thirty years. Jerry Pate Design has created original projects out of such ecologically sensitive environments as the swamps of coastal Florida, the desert sands of California, the coastal dunes of Alabama, the Rocky Mountains of the Lower Appalachians, the glacial plains of Michigan, and the long-leaf pine savannahs of Mississippi. Jerry Pate Design has also created beautiful golf landscapes from numerous environmentally degraded sites, such as depleted farmlands, phosphate mines, and residential landfills.</p>
                             <p>Jerry Pate Design designs golf courses for all levels of end user.  Jerry Pate courses have hosted the highest level of national professional tournaments, have supported world-renowned resorts, and have entertained large volumes of municipal recreational golfers. Jerry Pate Design’s vast history of experiences offers new clients an intelligent and versatile perspective when discovering the best solutions for each new original golf course design.</p>
                             <div class="heading-block nobottommargin">
-                         <h3></h3>
+                         <h2></h2>
                             <span>Renovation/Remodel</span></div>
                             <p>Jerry Pate Design has over twenty-five years of experience renovating or remodeling existing golf courses. JPD understands that the details of a project are crucial to its success no matter what scale the project. As a result, JPD is intimately involved in each project and has provided full golf course design services for any size renovation or remodel project from a small scale bunker restoration to a complete upgrade of an entire facility. For an existing club or golf course facing the need for improvements, JPD has the experience to provide complete studies of their needs and develop a master plan of improvements that can be implemented immediately or over time. </p>
 
@@ -132,7 +132,7 @@ include('partials/header-pages.php'); ?>
                             <p>Jerry Pate Design has acted as the lead consultant on many of its projects and has the experience to manage the design phase of any project for all areas of design and planning. In doing so, JPD has coordinated the services of qualified engineers, architects, and planners best suited for each project; and led the design team to facilitate permitting, coordinate functionality among facilities, and ensure aesthetic continuity for the project.</p>
                             <p>Jerry Pate also owned and operated a successful landscape construction company until 2003. During those operations, JPD gained the experience, personnel, and expertise to lead and supervise landscape contractors to produce premium efficiency with budgets and schedules, and to ensure the highest quality of construction. JPD’s program management experience includes preliminary analyses of cost, assistance in the bidding process and the selection of a general contractor or various trade contractors, and the administration and management of the contractors during construction.</p>
                          <div class="heading-block nobottommargin">
-                         <h3></h3>
+                         <h2></h2>
                             <span>Program Management</span></div>
                             <p>Jerry Pate also owned and operated a successful landscape construction company until 2003. During those operations, JPD gained the experience, personnel, and expertise to lead and supervise landscape contractors to produce premium efficiency with budgets and schedules, and to ensure the highest quality of construction. JPD’s program management experience includes preliminary analyses of cost, assistance in the bidding process and the selection of a general contractor or various trade contractors, and the administration and management of the contractors during construction.</p>
 
