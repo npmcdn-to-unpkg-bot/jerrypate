9208899bc80f8ba6e2f9c272e1af23556ef377c2
Anne Grundhoefer <annegrundhoefer@gmail.com>
2016-05-03 18:14:53 -0500

    made contact page editable

--- wp-content/themes/jerrypate/contact.php
+++ wp-content/themes/jerrypate/contact.php
@@ -12,62 +12,42 @@ include('partials/header-pages.php'); ?>
         <section id="page-title" class="page-title-mini">
 
             <div class="container clearfix">
-                <h1>Contact</h1>
-                <span>Get in Touch with Us</span>
+                <h1><?php the_title(); ?></h1>
             </div>
 
         </section><!-- #page-title end -->
 
         <!-- Content
         ============================================= -->
-        <section id="content">
 
-            
-<div class="content-wrap"
-                
+        <?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-<div id="section-contact" class="page-section notoppadding">
-	<div class="container clearfix">
-					<div class="row noleftmargin norightmargin bottommargin common-height">
-						<div class="col-md-8 col-sm-6">
-							<img class="image_fade" src="<?php bloginfo('template_url'); ?>/images/contact.jpg">
-						</div>
-						<div class="col-md-4 col-sm-6" style="background-color: #fff;">
-							<div class="max-height">
-								<h3 class="font-body t400 ls1">Locations</h3>
-
-								<div class="responsive-center">
-								<div style="font-size: 16px; line-height: 1.7;">
-									<address style="line-height: 1.7;">
-										<strong>Pensacola, FL</strong><br>
-										<a href="https://www.google.com/maps/place/301+Schubert+Dr,+Pensacola,+FL+32504/@30.4908381,-87.2315907,17z/data=!3m1!4b1!4m2!3m1!1s0x8890bffba7a23bb7:0x6720429809d6f25a" target="_blank">301 Schubert Drive<br>
-										Pensacola, FL 32504</a><br>
-									</address>
-
-									<address style="line-height: 1.7;">
-										<strong>Birmingham, AL</strong><br>
-										<a href="https://www.google.com/maps/place/604+28th+St+N,+Birmingham,+AL+35203/@33.5244948,-86.8001485,17z/data=!3m1!4b1!4m2!3m1!1s0x88891ba3ea8dcd8f:0xcf16a002752fb9a9" target="_blank">604 28th Street North<br>
-										Birmingham, AL 35203</a><br>
-									</address>
-
-									<abbr title="Phone Number"><strong>Phone:</strong></abbr> <a href="tel:+18007007001"> (800) 700-7001</a><br>
-									<!-- <abbr title="Fax"><strong>Hours:</strong></abbr>  M-F 8am-5pm<br> -->
-									<abbr title="Email Address"><strong>Email:</strong></abbr> <a href="mailto:jdp@jerrypate.com"> jdp@jerrypate.com</a>
-								</div>
-								</div>
+        	<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
+					
+			<?php endif; ?>
+<section id="content">
+	<div class="content-wrap"
+		<div id="section-contact" class="page-section notoppadding">
+			<div class="container clearfix">
+				<div class="row noleftmargin norightmargin bottommargin common-height">
+					<div class="col-md-8 col-sm-6">
+						<img class="image_fade" src="<?php the_post_thumbnail(); // Fullsize image for the single post ?>
+					</div>
+					<div class="col-md-4 col-sm-6" style="background-color: #fff;">
+						<div class="max-height">
+							<h3 class="font-body t400 ls1">Locations</h3>
+							<div class="responsive-center">
+							<?php the_content();?>
 							</div>
 						</div>
 					</div>
+				</div>
+			</div>				
+		</div>
+	<div class="container clearfix"></div>
+</section><!-- #content end -->
 
-					
-
-					<div class="container clearfix">
-
-            </div>
-
-        </section><!-- #content end -->
-
-        </section><!-- #content end -->
-
-
+						<?php endwhile; ?>
+	<?php else: ?>
+	<?php endif; ?>
 <?php include('partials/footer.php'); ?>
